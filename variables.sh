19670aebf413c4ffed8588f5ec0e628bbf168ce5
Kukunuri Prasanna <prasannakukunuri35@gmail.com>
2025-09-25 10:56:27 +0530

    variables

--- variables.sh
+++ variables.sh
@@ -4,7 +4,6 @@
 person1=$1
 person2=$2
 
-person1::hi,good morning
-person2::very good morning, person1.
-person1::have you had breakfast?
-person2::not yet, you?
+echo "person1::hi,good morning"
+echo "person2::very good morning, person1."
+echo "person1::have you had breakfast?"

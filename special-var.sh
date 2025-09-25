7f7aef0d6a09a9fd9301f5086ed09be82931e0e6
Kukunuri Prasanna <prasannakukunuri35@gmail.com>
2025-09-25 15:31:57 +0530

    special-arguments

--- special-var.sh
+++ special-var.sh
@@ -1,3 +1,8 @@
 #!/bin/bash/
 
 echo "All variables passed to the script: $@"
+echo "where am I: $PWD"
+echo "script name: $0"
+echo "PID of this script: $$"
+echo "who is running: $USER"
+echo "running home: $HOME

10da8190613efad4a70e35acfb0472089d708716
Kukunuri Prasanna <prasannakukunuri35@gmail.com>
2025-09-27 19:53:37 +0530

    conditions

---
+++ conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+number=$1
+
+if [$number -gt 0]
+ then
+    echo "Entered number is greater than $number"
+elif [$number -lt 0]
+ then
+    echo "Entered number is less than $number"
+else [$number -eq 0]
+ then
+    echo " Entered number is equal to $number"

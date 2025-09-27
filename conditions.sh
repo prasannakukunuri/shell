cd22e0e4d62e28cac43fe3fe708df771e6031488
Kukunuri Prasanna <prasannakukunuri35@gmail.com>
2025-09-27 20:11:29 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -2,13 +2,10 @@
 
 number=$1
 
-if [ $number -gt 0]
-then
+if [ $number -gt 0]; then
     echo "Entered number is greater than $number"
-elif [ $number -lt 0]
-then
+elif [ $number -lt 0]; then
     echo "Entered number is less than $number"
-else [ $number -eq 0]
-then
+else [ $number -eq 0]; then
     echo " Entered number is equal to $number"
 fi
